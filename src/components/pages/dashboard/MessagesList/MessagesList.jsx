2faf01a37574fec80eb5334de41840c347d7534b
Jordan DE GEA <jordan.de-gea@grenoble-inp.org>
2016-10-04 11:33:11 +0200

    Correction des replaces

--- src/components/pages/dashboard/MessagesList/MessagesList.jsx
+++ src/components/pages/dashboard/MessagesList/MessagesList.jsx
@@ -106,10 +106,11 @@ var MessagesListBlock = React.createClass({
   onDisplayMessage: function(object){
     var content = object.get("content");
 
-    content = content.replace("{js_jquery}","http://code.jquery.com/jquery-3.1.1.min.js");
-    content = content.replace("{js_jquery3.1.1}","http://code.jquery.com/jquery-3.1.1.min.js");
+    content = content.replace("{$js_jquery}","http://code.jquery.com/jquery-3.1.1.min.js");
+    content = content.replace("{$bootswatch_cerulean}", "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.7/cerulean/bootstrap.min.css");
+    content = content.replace("{$js_jquery3.1.1}","http://code.jquery.com/jquery-3.1.1.min.js");
     content = content.replace("{$bootstrap}","https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
-    content = content.replace("{js_bootstrap}","https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js");
+    content = content.replace("{$js_bootstrap}","https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js");
     
     this.setState({
       content: content
